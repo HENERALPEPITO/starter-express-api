ef595fa946f1001e8a3b79ee1d40783048448a35
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2023-10-07 21:28:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('BADING si KURT!!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
+});
+
+// Define a route for the homepage
+app.get('/', (req, res) => {
+    // HTML with styles
+    const htmlResponse = `
+        <html>
+            <head>
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f0f0f0;
+                    }
+                    h1 {
+                        color: #333;
+                    }
+                    p {
+                        color: #666;
+                    }
+                </style>
+            </head>
+            <body>
+                <h1>Welcome to My Express App</h1>
+                <p>This is a simple Express.js application with some styles applied.</p>
+            </body>
+        </html>
+    `;
+    res.send(htmlResponse);
+});
+
+// Handle 404 errors
+app.use((req, res) => {
+    res.status(404).send('404 - Not Found');
+});
+
+// Handle errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('500 - Internal Server Error');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
